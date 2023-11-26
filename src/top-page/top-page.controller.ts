e6f52a7c216641dbcaac4ffb23fae6b43c1bdd46
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-11-26 18:42:50 +0200

    mongo

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -14,7 +14,6 @@ import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
-  constructor(private readonly configService: ConfigService) {}
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
