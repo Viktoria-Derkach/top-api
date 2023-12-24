98b4e960e2cbeba4869c7560de4e7471f24df4a8
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-12-24 20:17:56 +0200

    autorithation and generationion of JWt

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED_ERROR = 'This user have been registered';
+export const USER_NOT_FOUND_ERROR = "The user with such email wasn't found";
+export const WRONG_PASSWORD_ERROR = 'wrong password';
